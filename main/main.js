7275acb032708d4fb38fea4914688ef6a4e33ad9
Aditya Samant <adityasamant2505@gmail.com>
2018-04-14 23:39:16 -0700

    Some fluff added to mainPage

--- main/main.js
+++ main/main.js
@@ -5,6 +5,9 @@ var loginNavBtn = document.getElementById('loginRef')
 
 var closeBtn= document.getElementsByClassName('closeBtn')[0]; //0 index since it gives us an elements arr.
 
+var index =0;
+slideShow(); //starts automatic slideshow
+
 //Set onclickListener for login modal
 loginNavBtn.addEventListener('click', openModal);
 
@@ -26,4 +29,18 @@ function outsideClick(e){
     if (e.target == modal){
         modal.style.display='none'; //closes it if we click the cursor outside of the box.    
     }
+}
+
+function slideShow(){
+  
+    var x = document.getElementsByClassName("Slideshow");
+    for (var i = 0; i < x.length; i++ ){
+        x[i].style.display="none"; //sets everything to invisible first
+    }
+    index ++;
+    if (index > x.length){
+        index= 1;
+    }
+    x[index-1].style.display="block"; //set an element to visible
+    setTimeout(slideShow,3000); //Change image every five seconds
 }
