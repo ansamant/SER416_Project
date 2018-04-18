a984c14101333b1b83b69668c1e1cdda3af94827
bepowell <bepowell@asu.edu>
2018-04-17 18:32:36 -0700

    some work on donate page

---
+++ main/donate.js
@@ -0,0 +1,41 @@
+//Get modal element
+var modal = document.getElementById('ModalView');
+
+var registerModal = document.getElementById('registerModal');
+//get login Navigation button
+var loginNavBtn = document.getElementById('loginRef');
+var registerButton = document.getElementById('registerRef');
+
+registerButton.onclick = function() {
+    registerModal.style.display = "block";
+};
+
+var closeBtn= document.getElementsByClassName('closeBtn')[0]; //0 index since it gives us an elements arr.
+var closeBtn2= document.getElementsByClassName('closeBtn')[1]; //0 index since it gives us an elements arr.
+
+
+//Set onclickListener for login modal
+loginNavBtn.addEventListener('click', openModal);
+
+//Set onClickListener for close modal
+closeBtn.addEventListener('click', closeModal);
+closeBtn2.addEventListener('click', closeModal);
+
+//outside click listener on window
+window.addEventListener('click', outsideClick);
+//open a Modal
+function openModal(){
+    modal.style.display='block';
+}
+
+function closeModal(){
+    modal.style.display='none';
+    registerModal.style.display='none';
+}
+
+function outsideClick(e){
+    if (e.target == modal){
+        modal.style.display='none'; //closes it if we click the cursor outside of the box.    
+    }
+}
+
