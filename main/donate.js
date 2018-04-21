20643260e9939a8bc46724a4c6318f16e49af703
bepowell <bepowell@asu.edu>
2018-04-21 14:56:14 -0700

    credit card entry for donations

--- main/donate.js
+++ main/donate.js
@@ -151,11 +151,165 @@ function donateSubmit(){
 	
 	var fNameField = document.getElementsByName("firstname")[0];
 	
+	clearElements();
+	showCreditCardEntry(fNameField);
+	
+	//showThanks(fNameField);
+}
+
+function clearElements(){
 	var x = document.getElementById("donateForm");
 	for (var i = x.childElementCount - 1; i > 0; i--){
 		x.removeChild(x.children[i])
 	}
+}
+
+function showCreditCardEntry(fNameField){
+	var x = document.getElementById("donateForm");
+	x.children[0].innerHTML = "Enter your Credit Card Information"
 	
+	var cardNameField = document.createElement("input");
+	cardNameField.placeholder = "Cardholder's Name"
+	cardNameField.style.marginRight = "25px";
+	cardNameField.style.height = "25px";
+	cardNameField.style.width = "222px";
+	cardNameField.style.marginBottom = "25px";
+	cardNameField.style.border = "1px solid gray";
+	
+	var cardNumField = document.createElement("input");
+	cardNumField.placeholder = "Card Number"
+	cardNumField.style.marginRight = "15px";
+	cardNumField.style.height = "25px";
+	cardNumField.style.width = "175px";
+	cardNumField.style.marginBottom = "25px";
+	cardNumField.style.border = "1px solid gray";
+	
+	var cardCvvField = document.createElement("input");
+	cardCvvField.placeholder = "CVV"
+	cardCvvField.maxLength = "3"
+	cardCvvField.style.height = "25px";
+	cardCvvField.style.width = "30px";
+	cardCvvField.style.border = "1px solid gray";
+	
+	var monthField = document.createElement("input");
+	monthField.placeholder = "mm"
+	monthField.style.height = "25px";
+	monthField.maxLength = "2"
+	monthField.size = "2"
+	monthField.style.width = "auto";
+	monthField.style.border = "1px solid gray";
+	
+	var slash = document.createElement("label");
+	slash.innerHTML = "/";
+	
+	var yearField = document.createElement("input");
+	yearField.placeholder = "yy"
+	yearField.style.height = "25px";
+	yearField.maxLength = "2"
+	yearField.size = "2"
+	yearField.style.width = "auto";
+	yearField.style.border = "1px solid gray";
+	
+	var divider = document.createElement("hr");
+	divider.className = "divider"
+	
+	var billingInfo = document.createElement("h2");
+	billingInfo.innerHTML = "Billing Information";
+	
+	var addr1 = document.createElement("input");
+	addr1.placeholder = "Address 1"
+	addr1.style.marginRight = "25px";
+	addr1.style.height = "25px";
+	addr1.style.width = "222px";
+	addr1.style.marginBottom = "25px";
+	addr1.style.border = "1px solid gray";
+	
+	var addr2 = document.createElement("input");
+	addr2.placeholder = "Address 2"
+	addr2.style.marginRight = "25px";
+	addr2.style.height = "25px";
+	addr2.style.width = "222px";
+	addr2.style.marginBottom = "25px";
+	addr2.style.border = "1px solid gray";
+	
+	var city = document.createElement("input");
+	city.placeholder = "City"
+	city.style.marginRight = "25px";
+	city.style.height = "25px";
+	city.style.width = "222px";
+	city.style.marginBottom = "25px";
+	city.style.border = "1px solid gray";
+	
+	var state = document.createElement("input");
+	state.placeholder = "State/province"
+	state.style.marginRight = "25px";
+	state.style.height = "25px";
+	state.style.width = "222px";
+	state.style.marginBottom = "25px";
+	state.style.border = "1px solid gray";
+	
+	var country = document.createElement("input");
+	country.placeholder = "Country"
+	country.style.marginRight = "25px";
+	country.style.height = "25px";
+	country.style.width = "222px";
+	country.style.marginBottom = "25px";
+	country.style.border = "1px solid gray";
+	
+	var zip = document.createElement("input");
+	zip.placeholder = "Zip/postal code"
+	zip.style.marginRight = "25px";
+	zip.style.height = "25px";
+	zip.style.width = "222px";
+	zip.style.marginBottom = "25px";
+	zip.style.border = "1px solid gray";
+	
+	var phone = document.createElement("input");
+	phone.placeholder = "Phone"
+	phone.style.marginRight = "25px";
+	phone.style.height = "25px";
+	phone.style.width = "222px";
+	phone.style.marginBottom = "25px";
+	phone.style.border = "1px solid gray";
+	
+	
+	var button = document.createElement("button");
+	button.innerHTML = "Make Donation"
+	button.className = "donate"
+	button.type = "button"
+	button.value = "reset"
+	button.addEventListener ("click", function() {
+		clearElements()
+		showThanks(fNameField)
+	});
+	
+	x.appendChild(cardNameField);
+	x.appendChild(document.createElement("br"));
+	x.appendChild(cardNumField);
+	x.appendChild(cardCvvField);
+	x.appendChild(document.createElement("br"));
+	x.appendChild(monthField);
+	x.appendChild(slash);
+	x.appendChild(yearField);
+	x.appendChild(divider);
+	
+	x.appendChild(billingInfo);
+	x.appendChild(addr1);
+	x.appendChild(addr2);
+	x.appendChild(document.createElement("br"));
+	x.appendChild(city);
+	x.appendChild(state);
+	x.appendChild(document.createElement("br"));
+	x.appendChild(country);
+	x.appendChild(zip);
+	x.appendChild(document.createElement("br"));
+	x.appendChild(phone);
+	x.appendChild(document.createElement("br"));
+	x.appendChild(button);
+}
+
+function showThanks(fNameField){
+	var x = document.getElementById("donateForm");
 	x.children[0].innerHTML = "Thank you for the $" + selectedAmount + " donation, " + fNameField.value +
 		". Every penny goes towards a good cause."
 	var button = document.createElement("button");
@@ -167,6 +321,7 @@ function donateSubmit(){
 		location.reload()
 	});
 	x.appendChild(button);
+	
 }
 
 function askQuestionPressed(){
